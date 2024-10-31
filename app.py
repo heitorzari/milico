bf83193888814a72b3b266d9958c773b192f8804
heitorzari <153790108+heitorzari@users.noreply.github.com>
2024-10-31 17:23:18 -0300

    arrumei o login

--- app.py
+++ app.py
@@ -18,6 +18,31 @@ def cadastro():
 
 
 
+usuarios = {
+    'admin' : 'admin',
+    'usuario' : 'senha',
+    'rafaela' : '111111',
+    'heitor' : '1271'
+}
+@app.route('/') #rota para a página inicial
+def home():
+    return render_template('index.html')
+
+@app.route('/login') #rota para a página de login
+def login():
+    return render_template('login.html')
+
+
+
+#verificar login
+@app.route('/verificar-login', methods=['POST   '])
+def verificar_login():
+   username = request.form['username']
+   password = request.form['password']
+
+    
 #huiu
 if __name__== '__main__':
     app.run(debug=True)
+
+    
